79455a42a35c237174a54f578128613b01074c27
Gene Grady <grady.gene.thomas@gmail.com>
2019-11-18 14:48:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+let text = document.getElementById(`text`)
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    text.textContent = "This is really cool!"
+});

b01ad95faee8ec8e6b5e467a33064ecb82dbf962
Amra Sezairi <amra.sezairi@hotmail.com>
2019-11-18 19:51:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  changeDOM()
+});
+
+function changeDOM() {
+    let a = document.getElementById('text')
+    a.innerHTML="This is really cool!"
+};
+
